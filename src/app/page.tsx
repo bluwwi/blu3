c8b559fe1dd245f63d83d35a94fc2b05e251c7ba
xRealBlue <heetvavadiya456@gmail.com>
2026-06-19 21:50:22 +0530

    back@

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -129,6 +129,14 @@ export default function Home() {
     <div className="relative w-full min-h-screen h-full  overflow-y-auto lg:overflow-hidden bg-[#131313] flex flex-col lg:flex-row">
       <div className="flex flex-col items-center justify-center w-full h-full min-h-screen">
         <Image
+          src={"/bg2.png"}
+          width={2000}
+          height={2000}
+          alt={"hello"}
+          className="w-[70%] opacity-50 rounded-3xl h-fit"
+        />
+
+        <Image
           src={"/logo/blu3.svg"}
           width={2000}
           height={2000}
