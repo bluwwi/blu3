8fdad7142c2c4e202281da7c70bb7fa5849b40aa
xRealBlue <heetvavadiya456@gmail.com>
2026-06-23 09:51:17 +0530

    @layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  viewportFit: "cover",psome-change
+  viewportFit: "cover",
   interactiveWidget: "resizes-content",
 };
 
