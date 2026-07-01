0285979eafee35d3422c639075f3e0fb3fd37270
xRealBlue <heetvavadiya456@gmail.com>
2026-07-01 10:15:33 +0530

    @roomloading

--- src/components/Player/ui/RoomLoading.tsx
+++ src/components/Player/ui/RoomLoading.tsx
@@ -6,17 +6,17 @@ import Image from "next/image";
 export function RoomLoading() {
   return (
     <div
-      className="min-h-dvh flex items-center justify-center"
+      className="min-h-dvh max-h-dvh h-[100vh] w-full fixed flex items-center justify-center"
       style={{ background: T.bg }}
     >
-      <div className="flex flex-col items-end">
+      <div className="flex flex-col w-full justify-center items-center">
         <Image
           width={400}
           height={400}
           src={"/logo/tvlogo.svg"}
           alt={"blu3"}
           priority
-          className={"w-24"}
+          className={"w-30"}
         />
       </div>
     </div>
