620ecac136ad98d3891310622f8720ad8c442e6a
xRealBlue <heetvavadiya456@gmail.com>
2026-07-01 15:04:47 +0530

    homebanner

--- src/components/ui/DiscordHomeBanner.tsx
+++ src/components/ui/DiscordHomeBanner.tsx
@@ -18,32 +18,12 @@ export function DiscordHomeBanner({
   const domain = new URL(url).hostname;
 
   return (
-    <div className="w-90 h-50 bg-linear-to-b from-blue-900 to-black p-4 relative">
-      {avatar && (
-        <div className="w-full flex flex-col justify-between h-full">
-          {/*<div className="absolute top-4 right-4 justify-end gap-2">
-            <img
-              src={avatar}
-              alt=""
-              className="w-14 h-14 border-2 border-white/70 rounded-full object-cover"
-            />
-          </div>*/}
-          <div className=""></div>
-          <div className="flex flex-col">
-            <span className="text-white text-center text-2xl">
-              Music, Fun {"&"} Vibe{"."}
-            </span>
-          </div>
-          <div className="w-full text-sm pr-1.5 text-right">{domain}</div>
-          <div className="absolute bottom-4 left-4 justify-end gap-2">
-            <img
-              src={"/logo/blu3.svg"}
-              alt=""
-              className="w-12 h-fit p-0.5 object-cover"
-            />
-          </div>
-        </div>
-      )}
+    <div className="w-90 h-50 relative">
+      <img
+        src={"/homebanner.png"}
+        alt=""
+        className="w-full h-full absolute inset-0 object-cover"
+      />
     </div>
   );
 }
