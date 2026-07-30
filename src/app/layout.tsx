9732d56f57019361bf7d08c441429f9a8dcfe743
xRealBlue <heetvavadiya456@gmail.com>
2026-07-30 20:41:19 +0530

    @favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/logo/blu3.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/publiclogo.png" sizes="512x512" />
+        <link rel="canonical" href="https://blu3.in" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
